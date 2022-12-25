78cd5c3674283000b7c4b221a719e76dc9cc3535
GitHub Action <action@github.com>
2022-12-25 17:25:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/sibnick/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sibnick/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sibnick.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sibnick.myplugin.MyBundle
 
 class MyApplicationService {
 
